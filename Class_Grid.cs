576a55f51e45a30d0d9413945c6e4a8fd1aa3e3c
WillBerrisford <willberrisforddev@gmail.com>
2018-05-12 00:53:32 +0100

    Major rewrite, trasnfered ship placement to Class_Grid from Class_Ship.

---
+++ Class_Grid.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Grid
+{
+
+	class Grid
+	{
+
+		private int Ships_left { get; set; }
+		private List<List<int>> Occupied_coords { get; set; }
+		private List<List<int>> Hit_coords { get; set; }
+		private List<List<int>> Miss_coords { get; set; }
+
+		public Grid()
+		{
+			Ships_left = 4;
+		}
+
+        //takes in 2d list of coords to be added, returns Occupied_coords with new coordinates added.
+		public List<List<int>> Place_ship(List<List<int>> Used_coords, List<List<int>> Occupied_coords) 
+		{
+
+			Occupied_coords[0].AddRange(Used_coords[0]); //These are the x coordinates
+			Occupied_coords[1].AddRange(Used_coords[1]); //These are the y coordinates
+			return Occupied_coords;
+            
+		}
+
+		//takes in a list of coords to be added, returns Hit_coords with new coordinates added.
+		public List<List<int>> Add_hit(List<int> Used_coords, List<List<int>> Hit_coords)
+		{
+
+			Hit_coords[0].Add(Used_coords[0]);//These are the x coordinates
+			Hit_coords[1].Add(Used_coords[1]);//These are the y coordinates
+			return Hit_coords;
+            
+		}
+
+		//takes in a list of coords to be added, returns Miss_coords with new coordinates added.
+		public List<List<int>> Add_miss(List<int> Used_coords, List<List<int>> Miss_coords)
+        {
+
+			Miss_coords[0].Add(Used_coords[0]);//These are the x coordinates
+			Miss_coords[1].Add(Used_coords[1]);//These are the y coordinates
+            return Miss_coords;
+
+        }
+
+        //
+		public List<List<int>> Generate_coords(int Rotation, List<int> Base_coords,int Length)
+		{
+        
+			List<List<int>> Generated_coords = new List<List<int>>();
+
+			switch (Rotation)
+			{
+				case 1:
+					Generated_coords[0] = Unchanging_coord(0, Base_coords, Length);
+					Generated_coords[1]= Changing_coord(1, Base_coords, Length, 1);
+					return Generated_coords;
+                    
+				case 3:
+					Generated_coords[0] = Unchanging_coord(0, Base_coords, Length);
+					Generated_coords[1] = Changing_coord(1, Base_coords, Length, -1);
+					return Generated_coords;
+    
+				case 2:
+					Generated_coords[1] = Unchanging_coord(1, Base_coords, Length);
+					Generated_coords[0] = Changing_coord(0, Base_coords, Length, 1);
+					return Generated_coords;
+                    
+				case 4:
+					Generated_coords[1] = Unchanging_coord(1, Base_coords, Length);
+					Generated_coords[0] = Changing_coord(0, Base_coords, Length, -1);
+					return Generated_coords;
+			}
+			return Generated_coords;
+		}
+
+		public List<int> Unchanging_coord(int X_or_y, List<int> Base_coords, int Length)
+		{
+			int Coordinate = Base_coords[X_or_y];
+			List<int> Unchanged_coords = new List<int>();
+
+			for (int num = 1; num <= Length; num++) 
+			{
+				Unchanged_coords.Add(Coordinate);
+			}
+
+			return Unchanged_coords;
+		}
+
+		public List<int> Changing_coord(int X_or_y, List<int> Base_coords, int Length, int Rotation_offset)
+		{
+          
+			int Coordinate = Base_coords[X_or_y];
+            List<int> Changed_coords = new List<int>();
+
+            for (int num = 1; num <= Length; num++)
+            {
+                Changed_coords.Add(Coordinate);
+				Coordinate = Coordinate + Rotation_offset;
+            }
+
+            return Changed_coords;
+		}
+        
+		//takes in a list of possible coordinates for ships and currently occupied coordinates,
+        //returns a bool value depending whether they are occupied or not and returns a 2d list of the first coordinates that are taken
+		public (bool, List<List<int>>) Check_coords(List<List<int>> Possible_coords, List<List<int>> Occupied_coords) //returns true if coordinates are occupied, false if they are not.
+		{
+			List<List<int>> taken_coords = new List<List<int>>();
+            
+			foreach (int x_coord in Possible_coords[0])  //runs through all x coordinates
+			{
+				if (Occupied_coords[0].Contains(x_coord)) //adds checks if x coordinate is present
+				{
+				
+					int index = Occupied_coords[0].IndexOf(x_coord); //finds index of x coorinate
+					if (Occupied_coords[1][index] == Possible_coords[1][index]) //checks whether the y coordinates in the Occupied_coords list and Possible_coords list is the same at the given index
+					{
+						taken_coords[0].Add(x_coord); //adds the taken x coordinate
+						taken_coords[1].Add(Possible_coords[1][index]); //adds the taken y coordinate
+						return (true, taken_coords);                       
+					}
+					else
+					{
+						
+						return (false,taken_coords);
+					}
+                    
+				}
+				else
+				{
+					break;
+				}
+			}
+			return (false, taken_coords);
+		}
+
+
+
+	}
+    
+}
---
+++ Class_Ship.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+namespace Class_Ship
+{
+
+    class Ship
+    {
+        //initializes all variables
+        private bool Sunk { get; set; } //whether ship was sunk or not
+        private int[,] Coordinates_taken { get; set; } //which coordinates the ship takes up
+        private int Ship_type { get; set; } //what type of ship the ship is out of (1,2,3,4)
+        private string Ship_type_string { get; set; } //what type of ship the ship is (Carrier, Battleship, Destroyer, Carrier
+        private int Rotation { get; set; } //the rotation offset (1,2,3,4)
+        private int Length { get; set; } //length of the ship in grid squares.
+
+        //default construtor, requires type of ship
+        public Ship(int Ship_type)
+        {
+            Sunk = false; //ships are not sunk when initialized
+            int type = Ship_type; //sets the type of ship
+
+            switch (type) //depending on type of ship a different label will be given
+            {
+                case 1: //if ship is type 1 (carrier)
+                    Length = 4;
+                    Ship_type_string = "Carrier";
+                    break;
+
+                case 2:
+                    Length = 3;
+                    Ship_type_string = "Battleship";
+                    break;
+
+                case 3:
+                    Length = 2;
+                    Ship_type_string = "Destroyer";
+                    break;
+
+                case 4:
+                    Length = 1;
+                    Ship_type_string = "Submarine";
+                    break;
+            }
+        }
+
+        //sets rotation function for setting rotation, requires user input
+        public int Set_rotation(int user_rotation)
+        {
+            Rotation = user_rotation;
+            return Rotation;
+        }
+
+        public bool Sunk_Ship()
+        {
+            Sunk = true;
+            return Sunk;
+        }
+    }
+
+}
